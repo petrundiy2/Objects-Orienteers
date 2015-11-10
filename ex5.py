e1da5d0f18220d505f70a2e983f8e05b610af6d9
petrundiy2 <petrundiy1@gmail.com>
2015-11-10 11:48:00 +0300

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,30 @@
+N=int(input())
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    def __mul__(self, other):
+        return Point(self.x*other.x,self.y*other.y)
+    def __sub__(self, other):
+        return Point(self.x-other.x,self.y-other.y)
+T=[]
+E=[]
+for u in range(N):
+    A=Point(int(input()),int(input()))
+    T+=str(A.x)
+    E+=str(A.y)
+m=0
+k=0
+n=0
+for u in range(len(T)):
+    for v in range(len(T)):
+        for b in range(len(T)):
+            if u!=v and u!=b and v!=b:
+                k+=((int(T[u])-int(T[v]))**2+(int(E[u])-int(E[v]))**2)**0.5+((int(T[u])-int(T[b]))**2+(int(E[u])-int(E[b]))**2)**0.5+((int(T[v])-int(T[b]))**2+(int(E[v])-int(E[b]))**2)**0.5
+                s=(k/2*(k/2-((int(T[u])-int(T[v]))**2+(int(E[u])-int(E[v]))**2)**0.5)*(k/2-((int(T[u])-int(T[b]))**2+(int(E[u])-int(E[b]))**2)**0.5)*(k/2-((int(T[u])-int(T[b]))**2+(int(E[u])-int(E[b]))**2)**0.5+((int(T[v])-int(T[b]))**2+(int(E[v])-int(E[b]))**2)**0.5))**0.5
+                if s>m:
+                    m=s
+                k=0
+print(m)
